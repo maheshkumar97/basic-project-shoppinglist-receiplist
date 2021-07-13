6d4812e5a83dc3c7b2e7e193606dae133238e6a5
maheshkumar97 <gonemaheshkumar@gmail.com>
2021-07-13 12:14:06 +0530

    Custom dropdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 
 @NgModule({
@@ -23,6 +24,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
     RecipeDetailsComponent,
     ShoppingEditComponent,
     ShoppingListComponent,
+    DropdownDirective,
   ],
   bootstrap: [AppComponent]
 })
